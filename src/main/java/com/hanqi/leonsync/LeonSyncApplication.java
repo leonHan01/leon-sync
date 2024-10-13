92ad89bad64974eb2eb297a75558c994e6174d6b
leonMacMini <leonhan1992@163.com>
2024-10-13 14:32:00 +0800

    first commit

---
+++ src/main/java/com/hanqi/leonsync/LeonSyncApplication.java
@@ -0,0 +1,13 @@
+package com.hanqi.leonsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeonSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanqi/leonsync/LeonSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanqi.leonsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
